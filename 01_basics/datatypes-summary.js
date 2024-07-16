926ae7d214bf3397e15a6c55eec7f4c1afe93f21
shwetabhore18 <shwetabhore95@gmail.com>
2024-07-16 05:50:46 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,49 @@
+// primitive (call bye value)
+
+// 7 types : string, Number, Boolean, Null, undefined, symbol, BigInt,
+
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId= Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 345678930298123n
+
+
+
+//  Reference (Non-Primitive) //returm type is object
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "shweta",
+    age: 22,
+}
+
+const myFunction = function(){
+    // console.log("Hello world");
+}
+
+console.log(typeof score) //->return type = number
+console.log(typeof scorevalue) //->return type = number
+console.log(typeof isLoggedIn) //->return type = boolean
+console.log(typeof outsideTemp) //->return type = object
+console.log(typeof userEmail) //->return type = undefined
+console.log(typeof id)         //->return type = symbol
+console.log(typeof anotherId)  //->return type = symbol
+console.log(typeof heros)      //->return type = object
+console.log(typeof myObj)      //->return type = object
+console.log(typeof myFunction) //->return type = function(object)
+
+// very imp this types asked in interviws
